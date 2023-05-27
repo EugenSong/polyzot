1a93c7e4995fc18edb2d7956d352849ea5065772
yasen002 <362yasen2@gmail.com>
2023-05-27 15:44:20 -0700

    added word image

--- components/Platform.js
+++ components/Platform.js
@@ -1,47 +1,53 @@
-import Matter from 'matter-js'
+import Matter from "matter-js";
 import React from "react";
-import { View } from "react-native";
-
+import { View, Image } from "react-native";
+import word1 from "../assets/words/food/banana-banana.png";
 let styles = {
-    platform: {
-        borderWidth: 1,
-        borderColor: 'black',
-        position: 'absolute',
-    }
-}
+  platform: {
+    borderWidth: 1,
+    borderColor: "black",
+    position: "absolute",
+  },
+};
 
-const Platform = ({ body, width, height }) => {
-    const { bounds, position } = body;
-    const widthBody = width
-    const heightBody = height
+const Platform = ({ body, width, height, style = {}, source }) => {
+  const { bounds, position } = body;
+  const widthBody = width;
+  const heightBody = height;
 
-    const x = position.x - widthBody / 2;
-    const y = position.y - heightBody / 2;
+  const x = position.x - widthBody / 2;
+  const y = position.y - heightBody / 2;
 
-    return (
-        <View style={[styles.platform, { width: widthBody, height: heightBody, left: x, top: y }]} />
-    );
-}
+  return (
+    <View
+      style={[
+        styles.platform,
+        { width: widthBody, height: heightBody, left: x, top: y },
+      ]}
+    >
+      <Image source={source} style={{ width, height, ...style }} />
+    </View>
+  );
+};
 
 export default (world, label, pos, size) => {
-    const initialPlatform = Matter.Bodies.rectangle(
-        pos.x,
-        pos.y,
-        size.width,
-        size.height,
-        {
-            label,
-            isStatic: true
-        }
-    )
-    Matter.World.add(world, initialPlatform)
-
-    return {
-        body: initialPlatform,
-        width: size.width,
-        height: size.height,
-        pos,
-        renderer: <Platform />
+  const initialPlatform = Matter.Bodies.rectangle(
+    pos.x,
+    pos.y,
+    size.width,
+    size.height,
+    {
+      label,
+      isStatic: true,
     }
-}
+  );
+  Matter.World.add(world, initialPlatform);
 
+  return {
+    body: initialPlatform,
+    width: size.width,
+    height: size.height,
+    source: word1,
+    renderer: <Platform />,
+  };
+};
--- entities.js
+++ entities.js
@@ -1,35 +1,39 @@
-import Matter from 'matter-js';
-import Platform from './components/Platform';
-import Peter from './components/Peter'
+import Matter from "matter-js";
+import Platform from "./components/Platform";
+import Peter from "./components/Peter";
 
-import { Dimensions } from 'react-native';
+import { Dimensions } from "react-native";
 
 const { width, height } = Dimensions.get("window");
 
-export default restart => {
-  let engine = Matter.Engine.create({ enableSleeping: false })
-  let world = engine.world
+export default (restart) => {
+  let engine = Matter.Engine.create({ enableSleeping: false });
+  let world = engine.world;
 
   engine.gravity.y = 0.4;
 
-  let platforms = {}
+  let platforms = {};
 
-  let spacing = 200
-  let startHeight = 400
+  let spacing = 200;
+  let startHeight = 400;
 
   for (let i = 0; i < 6; i++) {
-    label = 'platform' + i
-    x = 100
-    y = i * 200
-    platforms[label] = Platform(world, label, { x: x, y: y }, { height: 50, width: 150 })
+    label = "platform" + i;
+    x = 100;
+    y = i * 200;
+    platforms[label] = Platform(
+      world,
+      label,
+      { x: x, y: y },
+      { height: 50, width: 150 }
+    );
   }
 
   return {
     physics: { engine, world },
 
     Peter: Peter(world, { x: 200, y: 0 }, { height: 80, width: 100 }),
-    ...platforms
+    ...platforms,
     // platform1: Platform(world, 'platform1', { x: 200, y: 200 }, { height: 50, width: 200 }),
-  }
-}
-
+  };
+};
