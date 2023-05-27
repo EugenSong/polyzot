69f09c86fd61b13ebbbae3c0c2b56f3065404a40
Eugene <75242911+EugenSong@users.noreply.github.com>
2023-05-26 21:09:15 -0700

    First commit.

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+import React, { PureComponent } from "react";
+import { AppRegistry, StyleSheet, StatusBar } from "react-native";
+import { GameEngine } from "react-native-game-engine";
+import { Finger } from "./renderers";
+import { MoveFinger } from "./systems"
+
+export default class BestGameEver extends PureComponent {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <GameEngine
+        style={styles.container}
+        systems={[MoveFinger]}
+        entities={{
+          1: { position: [40, 200], renderer: <Finger /> }, //-- Notice that each entity has a unique id (required)
+          2: { position: [100, 200], renderer: <Finger /> }, //-- and a renderer property (optional). If no renderer
+          3: { position: [160, 200], renderer: <Finger /> }, //-- is supplied with the entity - it won't get displayed.
+          4: { position: [220, 200], renderer: <Finger /> },
+          5: { position: [280, 200], renderer: <Finger /> }
+        }}>
+
+        <StatusBar hidden={true} />
+
+      </GameEngine>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#FFF"
+  }
 });
+
+AppRegistry.registerComponent("BestGameEver", () => BestGameEver);
