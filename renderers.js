fb22c3a08140d0280c2cbe4e0f72ec9eb688d29e
braydenrudisill <braydenrudisill@gmail.com>
2023-05-27 12:28:18 -0700

    initial

--- renderers.js
+++ renderers.js
@@ -1,4 +1,3 @@
-import { GameEngine } from "react-native-game-engine";
 import React, { PureComponent } from "react";
 import { StyleSheet, View, Image } from "react-native";
 import peter from "./assets/petr-medium.png";
@@ -6,39 +5,34 @@ import peter from "./assets/petr-medium.png";
 const WIDTH = 100;
 const HEIGHT = 20;
 
-class Platform extends PureComponent {
-  render() {
-    const { position, width, height } = this.props;
-    const x = position[0] - WIDTH / 2;
-    console.log(width);
-    const y = position[1] - HEIGHT / 2;
-    return <View style={[styles.platform, { left: x, top: y }]} />;
-  }
+const Platform = ({ body }) => {
+  const { position } = body;
+  const x = position.x - WIDTH / 2;
+  const y = position.y - HEIGHT / 2;
+  return <View style={[styles.platform, { left: x, top: y }]} />;
 }
 
-class Petr extends PureComponent {
-  render() {
-    const { position } = this.props;
-    const x = position[0];
-    const y = position[1];
-    return (
-      <View style={{ left: x, top: y }}>
-        <Image source={peter} />
-      </View>
-    );
-  }
+const Peter = ({ body }) => {
+  const { position } = body;
+  const x = position.x - 50 / 2;
+  const y = position.y - 50 / 2;
+  return (
+    <View style={{ left: x, top: y }}>
+      <Image source={peter} />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
+const styles = {
   platform: {
-    borderColor: "#CCC",
-    borderWidth: 4,
-    // borderRadius: 0,
-    width: WIDTH * 2,
-    height: HEIGHT * 2,
-    backgroundColor: "pink",
+    backgroundColor: "#FF5877",
+    borderColor: "#FFC1C1",
+    borderWidth: 20,
+    width: WIDTH,
+    height: HEIGHT,
     position: "absolute",
+    borderRadius: 2
   },
-});
+};
 
-export { Platform, Petr };
+export { Platform, Peter };
