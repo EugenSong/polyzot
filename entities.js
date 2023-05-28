0fe1610300db258c5699d53ce6b620fefdc44a83
braydenrudisill <braydenrudisill@gmail.com>
2023-05-27 23:15:32 -0700

    f

--- entities.js
+++ entities.js
@@ -26,11 +26,11 @@ export default (restart) => {
   const invWrongImages = importAll(
     require.context("./assets/game-screen/animals/inverses", false, /\.(png)$/)
   );
-  console.log(wrongImages)
+
   let platforms = {};
 
   let spacing = 200;
-  let startHeight = 200;
+  let startHeight = 300;
   const platformWidth = 100;
   const platformHeight = 30;
 
@@ -38,8 +38,8 @@ export default (restart) => {
 
   for (let i = 0; i < 6; i++) {
     label = "platform" + i;
-    x = 70;
-    y = startHeight - i * 200;
+    x = 100;
+    y = startHeight - i * spacing;
     correct = correct_array[i]
     platforms[label] = Platform(
       world,
@@ -53,8 +53,8 @@ export default (restart) => {
   }
   for (let i = 0; i < 6; i++) {
     label = "platform2" + i;
-    x = 330;
-    y = startHeight - i * 200;
+    x = 300;
+    y = startHeight - i * spacing;
     correct = 1 - correct_array[i]
     platforms[label] = Platform(
       world,
