eac154469ad54bd7fca9296c1ec148348f203538
Eugene <75242911+EugenSong@users.noreply.github.com>
2023-05-27 21:57:35 -0700

    translations for food done.
    food platform translates.

--- assets/translations.js
+++ assets/translations.js
@@ -1,17 +1,12 @@
 
 
-
-const images = importAll(
-    require.context("./assets/game-screen/food", false, /\.(png)$/)
-);
-
 const translationsArr = [4, 5, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63];
 
 // map to store tranlation
 const translations = {};
 
 
-images.forEach((image, index) => {
+translationsArr.forEach((image, index) => {
 
     switch (translationsArr[index]) {
         case 4:
