8cec534a9f04312b6e31a4214520cf1219ec9924
yasen002 <362yasen2@gmail.com>
2023-05-27 20:50:26 -0700

    score board alignment

--- app/game.js
+++ app/game.js
@@ -1,55 +1,68 @@
 import { StyleSheet, TouchableOpacity, Text, View, Image } from "react-native";
-import React, { useState, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
 import { Link } from "expo-router";
-import { GameEngine } from 'react-native-game-engine';
+import { GameEngine } from "react-native-game-engine";
 
-import entities from '../entities';
-import { physics, updatePlatforms, updatePeter, checkForCollision } from '../physics';
+import entities from "../entities";
+import {
+  physics,
+  updatePlatforms,
+  updatePeter,
+  checkForCollision,
+} from "../physics";
 
 import ScoreBoard from "../ScoreBoard";
 
 export default function Page() {
-  const [running, setRunning] = useState(true)
-  const [gameEngine, setGameEngine] = useState(null)
-  const [currentPoints, setCurrentPoints] = useState(0)
+  const [running, setRunning] = useState(true);
+  const [gameEngine, setGameEngine] = useState(null);
+  const [currentPoints, setCurrentPoints] = useState(0);
   return (
     <>
-    {/* go back button */}
-    <View style={{ marginTop: 80, marginLeft: 10, }}>
-      <Link href="/selection">
-        <Image
-          source={require('../assets/backbutton.png')}
-          resizeMode="contain" />
-      </Link>
-    </View>
-    
-    <View style={{ flex: 1 }}>
+      {/* go back button */}
+      <View
+        style={{
+          marginTop: 80,
+          marginLeft: 10,
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Link href="/selection">
+          <Image
+            source={require("../assets/backbutton.png")}
+            resizeMode="contain"
+          />
+        </Link>
         <ScoreBoard score={currentPoints} />
+      </View>
 
+      <View style={{ flex: 1 }}>
         <GameEngine
-          ref={(ref) => { setGameEngine(ref); } }
+          ref={(ref) => {
+            setGameEngine(ref);
+          }}
           systems={[physics, updatePlatforms, updatePeter, checkForCollision]}
           entities={entities()}
           running={running}
           onEvent={(e) => {
             switch (e.type) {
-              case 'game_over':
+              case "game_over":
                 setRunning(false);
                 gameEngine.stop();
                 break;
-              case 'new_point':
+              case "new_point":
                 setCurrentPoints(currentPoints + 1);
                 break;
             }
-          } }
-          style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
-        >
-
-
-
-        </GameEngine>
-      </View></>
+          }}
+          style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}
+        ></GameEngine>
+      </View>
+    </>
   );
 }
 
